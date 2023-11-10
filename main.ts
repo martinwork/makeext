9aa18e8899f4e8ad8486876a09232f6a520c6f1f
Martin Williams <martinwork@users.noreply.github.com>
2023-11-10 01:07:03 +0000

    Update main.blocks, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-custom.foo(5, "Hello", MyEnum.One)
-basic.forever(function () {
-    custom.foo(3, "Hello", MyEnum.One)
-    custom.foo(2, "Hello", MyEnum.One)
-})
+/**
+ * custom.foo(0, "Hello", MyEnum.One)
+ * 
+ * main.ts
+ * 
+ * basic.forever(function () {
+ * 
+ * custom.foo(1, "Hello", MyEnum.One)
+ * 
+ * custom.foo(2, "Hello", MyEnum.One)
+ * 
+ * })
+ * 
+ * basic.forever(function () {
+ * 
+ * custom.foo(3, "Hello", MyEnum.One)
+ * 
+ * custom.foo(4, "Hello", MyEnum.One)
+ * 
+ * })
+ */
